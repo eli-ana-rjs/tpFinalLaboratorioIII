78721cfb013ff591e041df90f3c149444835bab0
msian0 <matias.siano@outlook.com>
2023-06-22 18:10:05 -0300

    refactor

--- src/main/java/org/utn/Repositorios/PlaylistPrivadaRepo.java
+++ src/main/java/org/utn/Repositorios/PlaylistPrivadaRepo.java
@@ -1,7 +1,9 @@
 package org.utn.Repositorios;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
-
+import org.utn.Models.Cancion;
+import org.utn.Models.Playlist;
 import org.utn.Models.PlaylistPrivada;
 
 import java.io.File;
@@ -11,32 +13,53 @@ import java.util.List;
 
 public class PlaylistPrivadaRepo implements IRepository<PlaylistPrivada> {
 
-    public final File archivoPlaylistPrivada= new File("src/main/java/org/utn/Archivos/playlistPrivada.json");
+    /**
+     * Atributo de tipo File que representa el archivo json donde se guardan las playlist privadas
+     */
+    public final File archivoPlaylistPrivada = new File("src/main/java/org/utn/Archivos/playlistPrivada.json");
+    /**
+     * Atributo de tipo ObjectMapper que se encarga de leer y escribir el archivo json
+     */
     public final ObjectMapper mapper = new ObjectMapper();
+    /**
+     * Atributo de tipo List que representa la lista de playlists privadas
+     */
     public List<PlaylistPrivada> listaPlaylistPrivada;
 
 
     //region Overrides
 
+    /**
+     * Metodo para cargar el Jason de Playlist Privadas
+     */
+
     @Override
     public void cargar() {
-        try{
+        try {
             CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(List.class, PlaylistPrivada.class);
             this.listaPlaylistPrivada = mapper.readValue(archivoPlaylistPrivada, collectionType);
-        } catch(IOException e){
+        } catch (IOException e) {
             this.listaPlaylistPrivada = new ArrayList<>();
         }
     }
 
+    /**
+     * Metodo para guardar en el Json de playlists privadas
+     */
     @Override
     public void guardar() {
-        try{
+        try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(archivoPlaylistPrivada, this.listaPlaylistPrivada);
-        } catch(IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Metodo para agregar una Playlist al Json de Playlist Privadas
+     *
+     * @param objeto
+     */
     @Override
     public void agregar(PlaylistPrivada... objeto) {
         cargar();
@@ -44,6 +67,11 @@ public class PlaylistPrivadaRepo implements IRepository<PlaylistPrivada> {
         guardar();
     }
 
+    /**
+     * Metodo que devuelve la lista de playlist privadas del Json
+     *
+     * @return lista de Playlist Privadas
+     */
     @Override
     public List<PlaylistPrivada> listar() {
         cargar();
@@ -55,11 +83,16 @@ public class PlaylistPrivadaRepo implements IRepository<PlaylistPrivada> {
         return false;
     }
 
+    /**
+     * Metodo que modifica una playlist privada del Json
+     *
+     * @param objeto
+     */
     @Override
     public void modificar(PlaylistPrivada objeto) {
         cargar();
-        for (PlaylistPrivada playlistPriv : listaPlaylistPrivada){
-            if (playlistPriv.equals(objeto)){
+        for (PlaylistPrivada playlistPriv : listaPlaylistPrivada) {
+            if (playlistPriv.equals(objeto)) {
                 playlistPriv.setNombre(objeto.getNombre());
                 playlistPriv.setListaCanciones(objeto.getListaCanciones());
                 playlistPriv.setIdCliente(objeto.getIdCliente());
@@ -69,6 +102,11 @@ public class PlaylistPrivadaRepo implements IRepository<PlaylistPrivada> {
         guardar();
     }
 
+    /**
+     * Metodo que elimina una playlist privada del Json
+     *
+     * @param objeto
+     */
     @Override
     public void eliminar(PlaylistPrivada objeto) {
         cargar();
--- src/main/java/org/utn/Repositorios/PlaylistPublicaRepo.java
+++ src/main/java/org/utn/Repositorios/PlaylistPublicaRepo.java
@@ -1,4 +1,5 @@
 package org.utn.Repositorios;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.type.CollectionType;
 import org.utn.Models.Cancion;
@@ -13,31 +14,52 @@ import java.util.List;
 
 public class PlaylistPublicaRepo implements IRepository<PlaylistPublica> {
 
-    public final File archivoPlaylistPublica= new File("src/main/java/org/utn/Archivos/playlistPublica.json");
+    /**
+     * Atributo de tipo File que representa el archivo json donde se guardan las playlist publicas
+     */
+    public final File archivoPlaylistPublica = new File("src/main/java/org/utn/Archivos/playlistPublica.json");
+    /**
+     * Atributo de tipo ObjectMapper que se encarga de leer y escribir el archivo json
+     */
     public final ObjectMapper mapper = new ObjectMapper();
+    /**
+     * Atributo de tipo List que representa la lista de playlists publicas
+     */
     public List<PlaylistPublica> listaPlaylistPublica;
 
 
     //region Overrides
+
+    /**
+     * Metodo para cargar el Jason de Playlist Publicas
+     */
     @Override
     public void cargar() {
-        try{
+        try {
             CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(List.class, PlaylistPublica.class);
             this.listaPlaylistPublica = mapper.readValue(archivoPlaylistPublica, collectionType);
-        } catch(IOException e){
+        } catch (IOException e) {
             this.listaPlaylistPublica = new ArrayList<>();
         }
     }
 
+    /**
+     * Metodo para guardar en el Json de playlists publicas
+     */
     @Override
     public void guardar() {
-        try{
+        try {
             mapper.writerWithDefaultPrettyPrinter().writeValue(archivoPlaylistPublica, this.listaPlaylistPublica);
-        } catch(IOException e){
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Metodo para agregar una Playlist al Json de Playlist Publicas
+     *
+     * @param objeto
+     */
     @Override
     public void agregar(PlaylistPublica... objeto) {
         cargar();
@@ -45,6 +67,11 @@ public class PlaylistPublicaRepo implements IRepository<PlaylistPublica> {
         guardar();
     }
 
+    /**
+     * Metodo que devuelve la lista de playlist publicas del Json
+     *
+     * @return lista de Playlist Publicas
+     */
     @Override
     public List<PlaylistPublica> listar() {
         cargar();
@@ -56,11 +83,16 @@ public class PlaylistPublicaRepo implements IRepository<PlaylistPublica> {
         return false;
     }
 
+    /**
+     * Metodo que modifica una playlist publica del Json
+     *
+     * @param objeto
+     */
     @Override
     public void modificar(PlaylistPublica objeto) {
         cargar();
-        for (PlaylistPublica playlistPub : listaPlaylistPublica){
-            if (playlistPub.equals(objeto)){
+        for (PlaylistPublica playlistPub : listaPlaylistPublica) {
+            if (playlistPub.equals(objeto)) {
                 playlistPub.setNombre(objeto.getNombre());
                 playlistPub.setListaCanciones(objeto.getListaCanciones());
                 break;
@@ -69,6 +101,11 @@ public class PlaylistPublicaRepo implements IRepository<PlaylistPublica> {
         guardar();
     }
 
+    /**
+     * Metodo que elimina una playlist publica del Json
+     *
+     * @param objeto
+     */
     @Override
     public void eliminar(PlaylistPublica objeto) {
         cargar();
